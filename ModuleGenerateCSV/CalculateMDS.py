ffc57368782eb09c01e8554fe7346412cb51c81b
unknown <alisonsassi@outlook.com>
2022-06-15 08:06:07 -0300

    Modified in files for generate CSV

---
+++ ModuleGenerateCSV/CalculateMDS.py
@@ -0,0 +1,110 @@
+import random
+from faker import Faker
+
+# Classe para gerar dados pessoais do Paciente (PATIENT,AGE)
+class PersonalData:
+    faker = Faker()
+    def patient():
+        return PersonalData.faker.name()
+    def age():
+        return random.randint(10,95)
+# Classe para gerar dados do SOFA curto prazo (NEUROLOGICAL,CARDIOVASCULAR,RESPIRATORY,COAGULATION,HEPATIC,RENAL)
+class ShortLifeForecast:
+    def neurological():
+        return random.randint(0,4)
+    def cardiovascular():
+        return random.randint(0,4)
+    def respiratory():
+        return random.randint(0,4)
+    def coagulation():
+        return random.randint(0,4)
+    def hepatic():
+        return random.randint(0,4)
+    def renal():
+        return random.randint(0,4)
+# Classe para gerar dados do SOFA longo prazo (ICC,ECOG)
+class LongLifeForecast:
+    def ICC():
+        return random.randrange(2, 5, 2)
+    def ECOG():
+        return random.randrange(2, 5, 2)
+# Classe para gerar dados do SOFA longo prazo (ICC,ECOG)
+class PersonalData:
+    faker = Faker()
+    def patient():
+        return PersonalData.faker.name()
+    def age():
+        return random.randint(10,95)
+
+class CalculateValuesOfPatient:
+    def calculateTotal(SOFA, ICC, AGE):
+
+        def calculatePointSOFA(SOFA):
+            if SOFA <= 8:
+                scorePointSOFA = 1
+            elif SOFA >= 9 and SOFA <= 11:
+                scorePointSOFA = 2    
+            elif SOFA >= 12 and SOFA <= 14:
+                scorePointSOFA = 3
+            elif SOFA > 14:
+                scorePointSOFA = 4
+            else:
+                print("the scorePointSOFA with problem")
+                scorePointSOFA = 1000
+
+            return scorePointSOFA
+
+        def calculatePointICC(ICC):
+
+            if ICC == 4:
+                scorePointICC = 3
+            else:
+                scorePointICC = 0
+            
+            return scorePointICC
+
+        def calculatePointAgeGroup(AGE):
+
+            if AGE <= 49:
+                scorePointAgeGroup = 1
+            elif AGE >= 50 and AGE <= 69:
+                scorePointAgeGroup = 2    
+            elif AGE >= 70 and AGE <= 84:
+                scorePointAgeGroup = 3
+            elif AGE >= 85:
+                scorePointAgeGroup = 4
+            else:
+                print("the scorePointAgeGroup with problem")
+                scorePointAgeGroup = 1000
+
+            return scorePointAgeGroup
+
+        scoreCalculateTotal = (
+                                calculatePointAgeGroup(AGE) + 
+                                calculatePointICC(ICC) + 
+                                calculatePointSOFA(SOFA)
+                            )
+
+        return scoreCalculateTotal
+
+    def calculateFragility(ECOG):
+
+        scoreFragility = ECOG
+
+        return scoreFragility
+
+    def calculateSOFA(NEUROLOGICAL,CARDIOVASCULAR,RESPIRATORY,COAGULATION,HEPATIC,RENAL):
+
+        scoreCalculateSOFA = (
+            int(NEUROLOGICAL) + 
+            int(CARDIOVASCULAR) +
+            int(RESPIRATORY) +
+            int(COAGULATION) +
+            int(HEPATIC) +
+            int(RENAL))
+
+        return scoreCalculateSOFA
+
+class CalculateClassification:
+    def Classification(SCORE_SOFA, SCORE_FRAGILITY, SCORE_TOTAL):
+        return 1
---
+++ ModuleGenerateCSV/GenerateFileDataBase.py
@@ -0,0 +1,81 @@
+import os
+import csv
+import CalculateMDS
+
+NameFileExport = 'data.csv'
+
+def createFile():
+    arq = open(NameFileExport,'w') 
+    arq.write('PATIENT,AGE,NEUROLOGICAL,CARDIOVASCULAR,RESPIRATORY,COAGULATION,HEPATIC,RENAL,ICC,ECOG,CALCULATE_MINOR_TOTAL,CALCULATE_FRAGILITY,CALCULATE_SOFA,CLASSIFICATION')
+    arq.close()
+
+def generateRegister():
+    patient = CalculateMDS.PersonalData.patient()
+    age = CalculateMDS.PersonalData.age()
+    neurological = CalculateMDS.ShortLifeForecast.neurological()
+    cardiovascular = CalculateMDS.ShortLifeForecast.cardiovascular()
+    respiratory = CalculateMDS.ShortLifeForecast.respiratory()
+    coagulation = CalculateMDS.ShortLifeForecast.coagulation()
+    hepatic = CalculateMDS.ShortLifeForecast.hepatic()
+    renal = CalculateMDS.ShortLifeForecast.renal()
+    icc = CalculateMDS.LongLifeForecast.ICC()
+    ecog = CalculateMDS.LongLifeForecast.ECOG()
+    scoreSOFA = CalculateMDS.CalculateValuesOfPatient.calculateSOFA(
+        NEUROLOGICAL = neurological,
+        CARDIOVASCULAR = cardiovascular,
+        RESPIRATORY= respiratory,
+        COAGULATION= coagulation,
+        HEPATIC= hepatic,
+        RENAL= renal
+        )
+    scoreFragility = CalculateMDS.CalculateValuesOfPatient.calculateFragility(ecog)
+    scoreMinorTotal = CalculateMDS.CalculateValuesOfPatient.calculateTotal(
+        SOFA= int(scoreSOFA),
+        ICC= int(icc),
+        AGE= int(age)
+        )
+    classification = CalculateMDS.CalculateClassification.Classification(
+        SCORE_SOFA = int(scoreSOFA),
+        SCORE_FRAGILITY = int(scoreFragility),
+        SCORE_TOTAL =int(scoreMinorTotal)
+        )
+    concatenationPersonalData = "%s,%s" % (str(patient), str(age))
+    concatenationShortLifeForecast = "%s,%s,%s,%s,%s,%s" % (
+        str(neurological),
+        str(cardiovascular),
+        str(respiratory),
+        str(coagulation),
+        str(hepatic),
+        str(renal)
+        )
+    concatenationLongLifeForecast = "%s,%s" % (str(icc), str(ecog))
+    concatenationCalculate = "%s,%s,%s,%s" % (
+        str(scoreMinorTotal),
+        str(scoreFragility),
+        str(scoreSOFA),
+        str(classification)
+        )
+    concatenation = "%s,%s,%s,%s" % (
+        str(concatenationPersonalData), 
+        str(concatenationShortLifeForecast),
+        str(concatenationLongLifeForecast),
+        str(concatenationCalculate)
+        )
+    return concatenation
+
+def insertOneRegisterInFile():
+    arq = open(NameFileExport,'a')
+    arq.write('\n')
+    dataGenerateRegister = generateRegister()
+    arq.write(dataGenerateRegister)
+    arq.close()
+
+# Main Program
+exist = os.path.exists(NameFileExport)
+if not (exist):
+    createFile()
+
+counter = 0
+while counter < 1000:
+  insertOneRegisterInFile()
+  counter += 1
