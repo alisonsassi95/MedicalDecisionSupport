bee6cd5e207b31eccafa1d1603cd6a85a5dab8b6
Alison Sassi <alisonsassi@outlook.com>
2022-08-28 22:08:39 -0300

    upload for heroku

--- MedicalDecisionSystem/settings.py
+++ MedicalDecisionSystem/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-# importing os module 
 import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
